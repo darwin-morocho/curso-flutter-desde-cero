c4f9ec2074f72ef83096f05e0f26f1f9397ee074
Darwin Morocho <darwin@MacBook-Pro-de-Darwin.local>
2020-03-29 21:55:19 -0500

    custom pageview

--- lib/pages/home_page_widgets/top_play_lists.dart
+++ lib/pages/home_page_widgets/top_play_lists.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app_2/models/play_list.dart';
 import 'package:my_flutter_app_2/utils/extras.dart';
@@ -14,11 +15,28 @@ class TopPlayLists extends StatefulWidget {
 }
 
 class _TopPlayListsState extends State<TopPlayLists> {
+  PageController _controller =
+      PageController(initialPage: 0, viewportFraction: 0.5);
   ValueNotifier<int> _currentPage = ValueNotifier<int>(0);
+
+  @override
+  void initState() {
+    super.initState();
+    // _controller.addListener(() {
+    //  // print("_controller.addListener");
+    // });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     print("build TopPlayLists");
+    final double itemWidth = MediaQuery.of(context).size.width * 0.75;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -30,66 +48,96 @@ class _TopPlayListsState extends State<TopPlayLists> {
           ),
         ),
         AspectRatio(
-          aspectRatio: 8 / 5,
+          aspectRatio: 7 / 4,
           child: Container(
-            child: PageView(
-              onPageChanged: (int page) {
-                _currentPage.value = page;
-              },
-              children: List.generate(widget.items.length, (int index) {
-                final PlayList item = widget.items[index];
-                return Container(
-                  margin: EdgeInsets.only(bottom: 10, left: 10, right: 10),
-                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                    BoxShadow(color: Colors.black12, blurRadius: 10)
-                  ]),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Expanded(
-                        child: CachedNetworkImage(
-                          imageUrl: item.banner,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              item.title,
-                              style: TextStyle(
-                                  fontSize: 17, fontWeight: FontWeight.bold),
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: NotificationListener(
+                onNotification: (t) {
+                  if (t is ScrollEndNotification) {
+                    final int page =
+                        (_controller.position.pixels / itemWidth).round();
+                    _currentPage.value = page;
+                  }
+                  return false;
+                },
+                child: ListView(
+                  controller: _controller,
+                  physics: CustomScrollPhysics(itemDimension: itemWidth),
+                  scrollDirection: Axis.horizontal,
+                  children: List.generate(widget.items.length, (int index) {
+                    final PlayList item = widget.items[index];
+                    return Container(
+                      width: itemWidth,
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      margin: EdgeInsets.only(bottom: 20),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(color: Colors.black12, blurRadius: 10)
+                          ]),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            child: CachedNetworkImage(
+                              imageUrl: item.banner,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
                             ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 Text(
-                                  Extras.fromNow(item.publishedAt),
+                                  item.title,
                                   style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w300),
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                                Text(
-                                  "Videos: ${item.itemCount}",
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w300),
-                                )
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      Extras.fromNow(item.publishedAt),
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    Text(
+                                      "Videos: ${item.itemCount}",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w300),
+                                    )
+                                  ],
+                                ),
                               ],
                             ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              }),
-            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }),
+                )),
           ),
         ),
+        // CupertinoButton(
+        //     child: Text("prev"),
+        //     onPressed: () {
+        //       // _controller.jumpTo((_currentPage.value + 1) * itemWidth);
+        //       _controller.animateTo((_currentPage.value - 1) * itemWidth,
+        //           duration: Duration(milliseconds: 300), curve: Curves.linear);
+        //     }),
+        // CupertinoButton(
+        //     child: Text("next"),
+        //     onPressed: () {
+        //       // _controller.jumpTo((_currentPage.value + 1) * itemWidth);
+        //       _controller.animateTo((_currentPage.value + 1) * itemWidth,
+        //           duration: Duration(milliseconds: 300), curve: Curves.linear);
+        //     }),
         ValueListenableBuilder(
             valueListenable: _currentPage,
             builder: (BuildContext _, int value, Widget w) {
@@ -126,4 +174,62 @@ class Dots extends StatelessWidget {
       ),
     );
   }
+}
+
+class CustomScrollPhysics extends ScrollPhysics {
+  final double itemDimension;
+
+  CustomScrollPhysics({this.itemDimension, ScrollPhysics parent})
+      : super(parent: parent);
+
+  @override
+  CustomScrollPhysics applyTo(ScrollPhysics ancestor) {
+    return CustomScrollPhysics(
+        itemDimension: itemDimension, parent: buildParent(ancestor));
+  }
+
+  double _getPage(ScrollPosition position, double portion) {
+    return (position.pixels + portion) / itemDimension;
+  }
+
+  double _getPixels(double page, double portion) {
+    return (page * itemDimension) - portion;
+  }
+
+  double _getTargetPixels(
+    ScrollPosition position,
+    Tolerance tolerance,
+    double velocity,
+    double portion,
+  ) {
+    double page = _getPage(position, portion);
+    if (velocity < -tolerance.velocity) {
+      page -= 0.5;
+    } else if (velocity > tolerance.velocity) {
+      page += 0.5;
+    }
+    return _getPixels(page.roundToDouble(), portion);
+  }
+
+  @override
+  Simulation createBallisticSimulation(
+      ScrollMetrics position, double velocity) {
+    // If we're out of range and not headed back in range, defer to the parent
+    // ballistics, which should put us back in range at a page boundary.
+    if ((velocity <= 0.0 && position.pixels <= position.minScrollExtent) ||
+        (velocity >= 0.0 && position.pixels >= position.maxScrollExtent))
+      return super.createBallisticSimulation(position, velocity);
+
+    final Tolerance tolerance = this.tolerance;
+    final portion = (position.extentInside - itemDimension) / 2;
+    final double target =
+        _getTargetPixels(position, tolerance, velocity, portion);
+    if (target != position.pixels)
+      return ScrollSpringSimulation(spring, position.pixels, target, velocity,
+          tolerance: tolerance);
+    return null;
+  }
+
+  @override
+  bool get allowImplicitScrolling => false;
 }
